6ee2d210a5d4306d48146061c1249325dbe150f9
Asanga Manage <asanga.lk@gmail.com>
2015-01-23 23:00:54 +1100

    Fixing issues

--- app/src/main/java/com/example/asanga/spinner/MainActivity.java
+++ app/src/main/java/com/example/asanga/spinner/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.asanga.spinner;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.asanga.spinner.adapters.NameValueSpinnerAdapter;
 import com.example.asanga.spinner.models.NameValue;
@@ -15,44 +16,46 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
-    private Spinner spinner;
+    private Spinner nameValueSpinner;
+    private TextView spinnerSelectionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameValueSpinner = (Spinner) findViewById(R.id.spinner);
+        spinnerSelectionTextView = (TextView) findViewById(R.id.spinnerSelectionTextView);
+        setupSpinner();
+    }
 
+    private void setupSpinner() {
+        // Setup data
+        List<NameValue> nameValues = createDummyValues();
+        NameValueSpinnerAdapter nameValueSpinnerAdapter = new NameValueSpinnerAdapter(this, R.layout.spinner_item, nameValues);
+        nameValueSpinner.setAdapter(nameValueSpinnerAdapter);
+
+        // Setup behaviour
+        nameValueSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                NameValue selection = (NameValue) nameValueSpinner.getItemAtPosition(position);
+                spinnerSelectionTextView.setText(selection.getName());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private List<NameValue> createDummyValues() {
         List<NameValue> nameValues = new ArrayList<>();
         nameValues.add(new NameValue("Samsung", "Galaxy"));
         nameValues.add(new NameValue("HTC", "Hero"));
         nameValues.add(new NameValue("Nokia", "Lumia"));
         nameValues.add(new NameValue("Apple", "Iphone"));
-
-        NameValueSpinnerAdapter nameValueSpinnerAdapter = new NameValueSpinnerAdapter(this, R.layout.spinner_item,nameValues);
-        spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(nameValueSpinnerAdapter);
+        return nameValues;
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/asanga/spinner/adapters/NameValueSpinnerAdapter.java
+++ app/src/main/java/com/example/asanga/spinner/adapters/NameValueSpinnerAdapter.java
@@ -1,6 +1,8 @@
 package com.example.asanga.spinner.adapters;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,41 +12,39 @@ import android.widget.TextView;
 import com.example.asanga.spinner.R;
 import com.example.asanga.spinner.models.NameValue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by asanga on 22/01/15.
+ * Created by Asanga Manage on 22/01/15.
  */
 public class NameValueSpinnerAdapter extends ArrayAdapter<NameValue> {
-
-    private int customeView;
+    private int customView;
     private LayoutInflater layoutInflater;
 
     public NameValueSpinnerAdapter(Context context, int resource, List<NameValue> objects) {
         super(context, resource, objects);
-        this.customeView = resource;
+        this.customView = resource;
         this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
-    public View getView(int position, View _convertView, ViewGroup parent) {
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return getView(position, convertView, parent);
+    }
 
-//        if (convertView == null) {
-//            convertView = layoutInflater.inflate(customeView, parent,false);
-//        }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-        View convertView = layoutInflater.inflate(R.layout.spinner_item,parent,false);
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(customView, parent, false);
+        }
+
         TextView nameTxtView = (TextView) convertView.findViewById(R.id.nameTextView);
         TextView valueTxtView = (TextView) convertView.findViewById(R.id.valueTextView);
-
-        if(position == 0){
-            nameTxtView.setText("-");
-            valueTxtView.setText("-");
-        }else {
-            NameValue nameValue = getItem(position);
-            nameTxtView.setText(nameValue.getName());
-            valueTxtView.setText(nameValue.getValue());
-        }
+        NameValue nameValue = getItem(position);
+        nameTxtView.setText(nameValue.getName());
+        valueTxtView.setText(nameValue.getValue());
         return convertView;
 
     }
